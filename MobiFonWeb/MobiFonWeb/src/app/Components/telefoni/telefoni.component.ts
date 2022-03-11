650610efb0da98bcf3d154ccc2d3c8091c924234
Admir Ljevo <admir.ljevo@edu.fit.ba>
2022-03-11 23:41:32 +0100

    ...

--- MobiFonWeb/MobiFonWeb/src/app/Components/telefoni/telefoni.component.ts
+++ MobiFonWeb/MobiFonWeb/src/app/Components/telefoni/telefoni.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MojConfig} from "../../moj-config";
 
 @Component({
   selector: 'app-telefoni',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelefoniComponent implements OnInit {
 
-  constructor() { }
+  telefonPodaci: any;
+
+  constructor(private httpKlijent: HttpClient) { }
 
   ngOnInit(): void {
+    this.preuzmiPodatke();
   }
 
+  preuzmiPodatke(){
+    this.httpKlijent.get(MojConfig.adresa_servera+"/Telefon/GetAll", this.telefonPodaci).subscribe(
+      (x: any)=>{
+        this.telefonPodaci=x;
+        alert("Podaci preuzeti");
+      }
+    )
+  }
+
+  obrisi(s: any) {
+
+  }
+
+  detalji(s: any) {
+
+  }
+
+  maticnaknjiga(s: any) {
+
+  }
 }
