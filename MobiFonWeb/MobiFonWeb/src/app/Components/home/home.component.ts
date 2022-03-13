a9bc564649c1b77f523d9bf1a2d87606af1cc886
Admir Ljevo <admir.ljevo@edu.fit.ba>
2022-03-13 03:30:43 +0100

    ...

--- MobiFonWeb/MobiFonWeb/src/app/Components/home/home.component.ts
+++ MobiFonWeb/MobiFonWeb/src/app/Components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {MojConfig} from "../../moj-config";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpKlijent: HttpClient, private router: Router) { }
+
+  brendovi: any;
+
+  preuzmiBrendove(){
+    this.httpKlijent.get(MojConfig.adresa_servera+"/Proizvodjac/GetAll", this.brendovi).subscribe(x=>{
+      this.brendovi=x;
+    })
+  }
 
   ngOnInit(): void {
+    this.preuzmiBrendove();
   }
 
 }
